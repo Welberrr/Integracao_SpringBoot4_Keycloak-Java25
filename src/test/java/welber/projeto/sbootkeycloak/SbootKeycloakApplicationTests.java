dbc7101df9aef5fdd5b3e895986d6c3bbd737084
Welberrr <welberhenrique.costa@gmail.com>
2026-03-06 23:31:22 -0300

    Feat: Inicializando projeto

---
+++ src/test/java/welber/projeto/sbootkeycloak/SbootKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package welber.projeto.sbootkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
